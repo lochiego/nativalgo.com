d1821669ddb31b511cec708e3d6bc1ee55351be3
Eric Gonzalez <erigonz89@gmail.com>
2022-10-09 00:13:24 -0500

    basic site

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      main: {
+        dark: "#26334E",
+        light: "#313A52",
+        text: "#CCCCCC",
+        card: "#36475F",
+        button: "#4C586C",
+      },
+    },
+  },
+  plugins: [],
+};
